8a7b50c5802a85c4cfecc1dd2fc1f10fecbc573a
AJ Perez <AnthonyJPerez24@gmail.com>
2015-06-21 09:27:03 -0400

    ptraceWrapper should now scan for entry and exit of munmap

--- ptrace_wipe_before_munmap/ptraceWiper/src/main.c
+++ ptrace_wipe_before_munmap/ptraceWiper/src/main.c
@@ -1,8 +1,14 @@
 
+#include <assert.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
+#include <sys/user.h>
 #include <sys/wait.h>
-#include <stdio.h>
 #include <unistd.h>
 #include "main.h"
 
@@ -11,14 +17,14 @@ int
 ptraceWrapper (
 	enum __ptrace_request request,
 	pid_t pid,
-	void * addr,
-	void * data
+	int addr,
+	int data
 )
 {
 	int status = 0;
 
 	errno = 0;
-	status = ptrace(request, pid, addr, data);
+	status = ptrace(request, pid, (void *)addr, (void *)data);
 	if (-1 == status)
 	{
 		fprintf(stderr, "Error during ptrace: %s\n", strerror(errno));
@@ -27,6 +33,7 @@ ptraceWrapper (
 
 	return status;
 }
+
 
 int
 attach (
@@ -39,7 +46,7 @@ attach (
 	result = ptraceWrapper(PTRACE_ATTACH, pid, 0, 0);
 	if (-1 == result)
 	{
-		fprintf(stderr, "Failed to attach to pid %d\n", pid);
+		fprintf(stderr, "Failed to attach to pid %d - %s\n", pid, strerror(errno));
 		return -1;
 	}
 
@@ -50,7 +57,7 @@ attach (
 		return -1;
 	}
 
-	if ((pid != result) || (!WIFSTOPPED(status))
+	if ((pid != result) || (!WIFSTOPPED(status)))
 	{
 		fprintf(stderr, "Unexpected status %d, result: %d\n", status, result);
 		return -1;
@@ -67,12 +74,12 @@ detach (
 	const pid_t pid
 )
 {
-	int status = -1;
+	int result = -1;
 
 	result = ptraceWrapper(PTRACE_DETACH, pid, 0, 0);
 	if (-1 == result)
 	{
-		fprintf(stderr, "Failed to detach from pid %d\n", pid);
+		fprintf(stderr, "Failed to detach from pid %d - %s\n", pid, strerror(errno));
 		return -1;
 	}
 
@@ -89,24 +96,112 @@ getPid (void)
 	int numRead = 0;
 	
 	printf("Enter the PID of the target executable: ");
-	numRead = scanf("%d", pid);
+	numRead = scanf("%d", (int *)&pid);
 	
 	assert(1 == numRead);
 	return pid;	
 }
 
 
-void
+void preAnalyzeSyscall(
+	const pid_t pid
+)
+{
+	int sys_call_nr = 0;
+	struct user_regs_struct regs;
+
+	memset(&regs, 0, sizeof(regs));
+	ptraceWrapper(PTRACE_GETREGS, pid, 0, (int)&regs);
+
+#ifdef __i386__
+		sys_call_nr = regs.orig_eax;
+#elif __x86_64__
+		sys_call_nr = regs.orig_rax;
+#else
+#error "Unsupported architecture."
+#endif
+
+	if (SYS_munmap == sys_call_nr)
+	{
+		printf("About to enter munmap()\n");
+	}
+}
+
+
+void postAnalyzeSyscall (
+	const pid_t pid
+)
+{
+	int sys_call_nr = 0;
+	struct user_regs_struct regs;
+
+	memset(&regs, 0, sizeof(regs));
+	ptraceWrapper(PTRACE_GETREGS, pid, 0, (int)&regs);
+
+#ifdef __i386__
+		sys_call_nr = regs.orig_eax;
+#elif __x86_64__
+		sys_call_nr = regs.orig_rax;
+#else
+#error "Unsupported architecture."
+#endif
+
+	if (SYS_munmap == sys_call_nr)
+	{
+		printf("Returned from munmap()\n");
+	}
+}
+
+
+int
 scanSyscalls (
 	const pid_t pid
 )
 {
 	bool done = false;
+	int result = 0;
+	int lastSignal = 0;
+	bool isSyscallEntry = true;
+
+	ptraceWrapper(PTRACE_SETOPTIONS, pid, 0, 
+		PTRACE_O_TRACESYSGOOD | PTRACE_O_TRACEEXIT);
 
 	while (!done)
 	{
-		
+		// Continue the process until the next syscall
+		result = ptraceWrapper(PTRACE_SYSCALL, pid, 0, lastSignal);
+		if (-1 == result)
+		{
+			fprintf(stderr, "Failed during PTRACE_SYSCALL - %s\n", strerror(errno));
+			return -1;
+		}
+
+		result = waitpid(pid, &lastSignal, WUNTRACED | WCONTINUED);
+		if (-1 == result)
+		{
+			fprintf(stderr, "Waitpid failed: %s\n", strerror(errno));
+			return -1;
+		}
+
+		//if (status == (SIGTRAP | PTRACE_EVENT_EXIT << 8))
+		//{
+		//	fprintf(stderr, "SIGTRAP reached\n");
+	    //    break;
+	    //}
+
+	    if (isSyscallEntry)
+	    {
+	    	preAnalyzeSyscall(pid);
+	    }
+	    else
+	    {
+	    	postAnalyzeSyscall(pid);
+	    }
+
+	    isSyscallEntry = !isSyscallEntry;
 	}
+
+	return 0;
 }
 
 
@@ -121,10 +216,12 @@ int main (int argc, char * argv[])
 	// Attach to the process
 	attach(pid);
 
+	// Look for munmap
+	scanSyscalls(pid);
+
 	// Allow the process to resume
 	detach(pid);
 
 	printf("Exiting...\n");
 	return 0;
 }
-
--- ptrace_wipe_before_munmap/testApplication/src/main.c
+++ ptrace_wipe_before_munmap/testApplication/src/main.c
@@ -10,83 +10,82 @@
 
 void * 
 getNewMap (
-	size_t size
+    size_t size
 )
 {
-	void * memoryMapPtr = NULL;
-	
-	assert (size != 0);
+    void * memoryMapPtr = NULL;
+    
+    assert (size != 0);
 
-	memoryMapPtr = mmap (
-			NULL, 	// Let the OS choose our address
-			size, 	// Map size
-			PROT_READ | PROT_WRITE,// Memory Permissions
-			MAP_SHARED | MAP_ANONYMOUS, // flags
-			-1,	// File Descriptor is -1 for anonymous maps
-			0);	// offset is ignored for anonymous maps
-	
-	if (MAP_FAILED == memoryMapPtr)
-	{
-		fprintf(stderr, "Error during mmap: %s\n", strerror(errno));
-		return NULL;
-	}
+    memoryMapPtr = mmap (
+            NULL,    // Let the OS choose our address
+            size,    // Map size
+            PROT_READ | PROT_WRITE,        // Memory Permissions
+            MAP_SHARED | MAP_ANONYMOUS,    // flags
+            -1,        // File Descriptor is -1 for anonymous maps
+            0);        // offset is ignored for anonymous maps
+    
+    if (MAP_FAILED == memoryMapPtr)
+    {
+        fprintf(stderr, "Error during mmap: %s\n", strerror(errno));
+        return NULL;
+    }
 
-	printf("Memory map created at %p\n", memoryMapPtr);
-	return memoryMapPtr;
+    printf("Memory map created at %p\n", memoryMapPtr);
+    return memoryMapPtr;
 }
 
 
 void
 freeMap (
-	void * memoryMapPtr,
-	size_t size
+    void * memoryMapPtr,
+    size_t size
 )
 {
-	assert (NULL != memoryMapPtr);
-	assert(size != 0);
+    assert (NULL != memoryMapPtr);
+    assert(size != 0);
 
-	int status = munmap(memoryMapPtr, size);
+    int status = munmap(memoryMapPtr, size);
 
-	if (-1 == status)
-	{
-		fprintf(stderr, "Error during munmap: %s\n", strerror(errno));
-		return;
-	}	
+    if (-1 == status)
+    {
+        fprintf(stderr, "Error during munmap: %s\n", strerror(errno));
+        return;
+    }    
 
-	printf("Freed the memory map of size %zu located at %p\n", size, memoryMapPtr);
+    printf("Freed the memory map of size %zu located at %p\n", size, memoryMapPtr);
 }
 
 
 int main (int argc, char * argv[])
 {
-	bool done = false;
-	printf("Starting TestApplication\n");
+    bool done = false;
+    printf("Starting TestApplication\n");
 
-	do
-	{
-		printf("\nHit a key to mmap then munmap (Q to quit)\n");
-		int selection = getchar();
-		getchar(); // Clear the newline
+    do
+    {
+        printf("\nHit a key to mmap then munmap (Q to quit)\n");
+        int selection = getchar();
+        getchar(); // Clear the newline
 
-		switch (selection)
-		{
-			case 'q':
-			case 'Q':
-				done = true;
-				break;
+        switch (selection)
+        {
+            case 'q':
+            case 'Q':
+                done = true;
+                break;
 
-			default:
-				{
-					int mapSize = 100;
-					void * memoryMapPtr = getNewMap(mapSize);
-					freeMap(memoryMapPtr, mapSize);
-				}
-				break;
-		}
+            default:
+                {
+                    int mapSize = 100;
+                    void * memoryMapPtr = getNewMap(mapSize);
+                    freeMap(memoryMapPtr, mapSize);
+                }
+                break;
+        }
 
-	} while (!done);
-	
-	printf("Exiting...\n");
-	return 0;
+    } while (!done);
+    
+    printf("Exiting...\n");
+    return 0;
 }
-
